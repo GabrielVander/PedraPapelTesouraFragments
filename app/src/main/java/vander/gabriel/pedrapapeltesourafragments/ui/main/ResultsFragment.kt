a7ad218ee4ba20cf279d5106b0378a430af3ff8f
Gabriel Vander dos Santos Guimarães <38587412+GabrielVander@users.noreply.github.com>
2022-02-24 19:52:57 -0300

    #7 Stated correctly displaying player's hand

--- app/src/main/java/vander/gabriel/pedrapapeltesourafragments/ui/main/ResultsFragment.kt
+++ app/src/main/java/vander/gabriel/pedrapapeltesourafragments/ui/main/ResultsFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import androidx.fragment.app.activityViewModels
 import vander.gabriel.pedrapapeltesourafragments.R
+import vander.gabriel.pedrapapeltesourafragments.ui.main.domain.Hand
 
 class ResultsFragment : Fragment() {
 
@@ -14,10 +17,27 @@ class ResultsFragment : Fragment() {
         fun newInstance() = ResultsFragment()
     }
 
+    private val mainViewModel: MainViewModel by activityViewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_results, container, false)
+        val view = inflater.inflate(R.layout.fragment_results, container, false)
+
+        mainViewModel.game.observe(viewLifecycleOwner) {
+            val playerHandIllustration: ImageView =
+                view.findViewById<View>(R.id.resultsPlayerHandIllustration) as ImageView
+
+            val handResource: Int = when (it.playerHand!!) {
+                Hand.ROCK -> R.drawable.ic_rock
+                Hand.PAPER -> R.drawable.ic_paper
+                Hand.SCISSORS -> R.drawable.ic_scissors
+            }
+
+            playerHandIllustration.setImageResource(handResource)
+        }
+
+        return view
     }
 }
