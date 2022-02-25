bc64bc4573cc3878470cc5fc22d054276aae0f5e
Gabriel Vander dos Santos Guimarães <38587412+GabrielVander@users.noreply.github.com>
2022-02-25 19:48:49 -0300

    #7 Implemented view binding usage for HandPickingFragment.kt

--- app/src/main/java/vander/gabriel/pedrapapeltesourafragments/ui/main/fragments/HandPickingFragment.kt
+++ app/src/main/java/vander/gabriel/pedrapapeltesourafragments/ui/main/fragments/HandPickingFragment.kt
@@ -4,10 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import vander.gabriel.pedrapapeltesourafragments.R
+import vander.gabriel.pedrapapeltesourafragments.databinding.FragmentHandPickingBinding
 import vander.gabriel.pedrapapeltesourafragments.ui.main.models.Hand
 import vander.gabriel.pedrapapeltesourafragments.ui.main.view_models.MainViewModel
 
@@ -17,29 +16,27 @@ class HandPickingFragment : Fragment() {
         fun newInstance() = HandPickingFragment()
     }
 
+    private lateinit var binding: FragmentHandPickingBinding
+
     private val mainViewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_hand_picking, container, false)
+    ): View {
+        binding = FragmentHandPickingBinding.inflate(layoutInflater)
 
-        val rockButton: ImageButton =
-            view.findViewById<View>(R.id.handPickingRockButton) as ImageButton
-        rockButton.setOnClickListener {
+        val view = binding.root
+
+        binding.handPickingRockButton.setOnClickListener {
             mainViewModel.setPlayerHand(Hand.ROCK)
         }
 
-        val paperButton: ImageButton =
-            view.findViewById<View>(R.id.handPickingPaperButton) as ImageButton
-        paperButton.setOnClickListener {
+        binding.handPickingPaperButton.setOnClickListener {
             mainViewModel.setPlayerHand(Hand.PAPER)
         }
 
-        val scissorsButton: ImageButton =
-            view.findViewById<View>(R.id.handPickingScissorsButton) as ImageButton
-        scissorsButton.setOnClickListener {
+        binding.handPickingScissorsButton.setOnClickListener {
             mainViewModel.setPlayerHand(Hand.SCISSORS)
         }
 
