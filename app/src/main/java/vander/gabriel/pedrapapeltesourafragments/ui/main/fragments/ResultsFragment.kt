886461eff8536b92c7e7a07a682076bede46bfc7
Gabriel Vander dos Santos Guimarães <38587412+GabrielVander@users.noreply.github.com>
2022-02-25 19:55:31 -0300

    #7 Implemented view binding usage for ResultsFragment.kt

--- app/src/main/java/vander/gabriel/pedrapapeltesourafragments/ui/main/fragments/ResultsFragment.kt
+++ app/src/main/java/vander/gabriel/pedrapapeltesourafragments/ui/main/fragments/ResultsFragment.kt
@@ -4,12 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import vander.gabriel.pedrapapeltesourafragments.R
+import vander.gabriel.pedrapapeltesourafragments.databinding.FragmentResultsBinding
 import vander.gabriel.pedrapapeltesourafragments.ui.main.view_models.MainViewModel
 
 class ResultsFragment : Fragment() {
@@ -18,33 +15,30 @@ class ResultsFragment : Fragment() {
         fun newInstance() = ResultsFragment()
     }
 
+    private lateinit var binding: FragmentResultsBinding
+
     private val mainViewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_results, container, false)
+    ): View {
+        binding = FragmentResultsBinding.inflate(layoutInflater)
 
-        val exitButton: Button =
-            view.findViewById<View>(R.id.resultsExitButton) as Button
+        val view = binding.root
 
-        exitButton.setOnClickListener {
+        binding.resultsExitButton.setOnClickListener {
             mainViewModel.resetGame()
         }
 
         mainViewModel.game.observe(viewLifecycleOwner) { game ->
-            val playerHandIllustration: ImageView =
-                view.findViewById<View>(R.id.resultsPlayerHandIllustration) as ImageView
-
-            val resultDisplay: TextView =
-                view.findViewById<View>(R.id.resultsText) as TextView
-
-            game.playerHand?.let { hand ->
-                playerHandIllustration.setImageResource(hand.iconResourceId)
+            game.playerHand?.let {
+                binding.resultsPlayerHandIllustration.setImageResource(it.iconResourceId)
             }
 
-            game.result?.let { resultDisplay.setText(it.displayResourceId) }
+            game.result?.let {
+                binding.resultsText.setText(it.displayResourceId)
+            }
         }
 
         return view
